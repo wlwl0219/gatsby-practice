c41912ab32890a389d02878172c59e3e30686aea
wlwl0219 <wlwl0219@gmail.com>
2023-04-11 14:44:56 +0900

    page Query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import Layout from "../components/Layout";
 import { Seo } from "../components/Seo";
+import { PageProps, graphql } from "gatsby";
 
-export default function Blog() {
+export default function Blog({ data }: PageProps<Queries.BlogTitlesQuery>) {
   return (
     <Layout title="Blog">
-      <div>
-        <p>The most recent news from my shop.</p>
-      </div>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
