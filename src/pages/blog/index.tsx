e9e51bc14e1a864f6d946425b1f570a02c55d828
wlwl0219 <wlwl0219@gmail.com>
2023-04-14 15:41:37 +0900

    static img

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import Layout from "../../components/Layout";
 import { Seo } from "../../components/Seo";
 import { Link, PageProps, graphql } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 export default function Blog({ data }: PageProps<Queries.BlogPostsQuery>) {
   return (
     <Layout title="Blog">
+      <StaticImage
+        src="https://images.unsplash.com/photo-1625768376503-68d2495d78c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80"
+        alt="sticker"
+      />
       <section>
         {data.allMdx.nodes.map((file, index) => (
           <article key={index}>
